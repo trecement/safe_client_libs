274eb18f4fdb883487466f9c0f5dabd69c938cf3
MaidSafe-QA <qa@maidsafe.net>
2016-06-28 13:45:31 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Pattern.js
+++ master/implementors/core/str/pattern/trait.Pattern.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['serde'] = [];implementors['socket_addr'] = [];implementors['tmp_mio'] = [];implementors['mio'] = [];implementors['hyper'] = [];implementors['igd'] = [];
+implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['serde'] = [];implementors['hyper'] = [];implementors['maidsafe_utilities'] = [];implementors['igd'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/std/ascii/trait.AsciiExt.js
+++ master/implementors/std/ascii/trait.AsciiExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['socket_addr'] = [];implementors['tmp_mio'] = [];implementors['mio'] = [];
+implementors['mio'] = [];implementors['tmp_mio'] = [];implementors['maidsafe_utilities'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
