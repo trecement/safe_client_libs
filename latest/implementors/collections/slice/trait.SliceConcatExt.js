0e1e1c112ef60c94aeac5259fb0cf837bc898c1b
MaidSafe-QA <qa@maidsafe.net>
2016-09-28 19:47:03 +0000

    Updated documentation.

--- latest/implementors/collections/slice/trait.SliceConcatExt.js
+++ latest/implementors/collections/slice/trait.SliceConcatExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["antidote"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["antidote"] = [];implementors["serde"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/collections/slice/trait.SliceConcatExt.js
+++ master/implementors/collections/slice/trait.SliceConcatExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["antidote"] = [];implementors["regex_syntax"] = [];implementors["humantime"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["antidote"] = [];implementors["serde"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
