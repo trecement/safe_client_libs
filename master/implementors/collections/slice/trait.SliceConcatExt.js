60eae5bceb087ab3a5d7581debb7929b37b4d7be
MaidSafe-QA <qa@maidsafe.net>
2016-10-03 12:03:12 +0000

    Updated documentation.

--- master/implementors/collections/slice/trait.SliceConcatExt.js
+++ master/implementors/collections/slice/trait.SliceConcatExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["antidote"] = [];implementors["regex_syntax"] = [];implementors["humantime"] = [];implementors["serde"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["antidote"] = [];implementors["regex_syntax"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/std/sys/ext/fs/trait.DirEntryExt.js
+++ master/implementors/std/sys/ext/fs/trait.DirEntryExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["antidote"] = [];implementors["humantime"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["antidote"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
