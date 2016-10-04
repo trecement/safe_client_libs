ad0547873490633c8f720f2bb5ce0a1fb5f5dfc7
MaidSafe-QA <qa@maidsafe.net>
2016-10-04 11:09:47 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.BitOrAssign.js
+++ master/implementors/core/ops/trait.BitOrAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["humantime"] = [];implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["libc"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["lazy_static"] = [];implementors["humantime"] = [];implementors["antidote"] = [];implementors["libc"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["rust_sodium_sys"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
