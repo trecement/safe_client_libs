b9b5f0ee6731238c370fdb5527e0e5cddaeddf53
MaidSafe-QA <qa@maidsafe.net>
2016-10-18 19:27:09 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.ShlAssign.js
+++ master/implementors/core/ops/trait.ShlAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["humantime"] = [];implementors["antidote"] = [];implementors["libc"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["rust_sodium_sys"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["hyper"] = [];implementors["rust_sodium_sys"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["antidote"] = [];implementors["libc"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["humantime"] = [];implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
