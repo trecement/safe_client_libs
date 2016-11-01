4f695e76945e08263816a82a62fc9c3a63af6637
MaidSafe-QA <qa@maidsafe.net>
2016-11-01 10:44:34 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ master/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["regex_syntax"] = [];implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["either"] = [];implementors["mio"] = [];implementors["tmp_mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
+implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["either"] = [];implementors["regex_syntax"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["tmp_mio"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];implementors["igd"] = [];implementors["maidsafe_utilities"] = [];implementors["crust"] = [];implementors["safe_core"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
